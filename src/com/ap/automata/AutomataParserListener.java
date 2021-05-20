2f1fa80b416703b1ce0694e541790d7eed5540d1
braggs <emielvdbrink@gmail.com>
2021-05-20 16:56:13 +0200

    changed stack type to Float, added multiplication and division

--- src/com/ap/automata/AutomataParserListener.java
+++ src/com/ap/automata/AutomataParserListener.java
@@ -6,32 +6,50 @@ import com.ap.antlr.base.AutomataParserBaseListener;
 import java.util.Stack;
 
 public class AutomataParserListener extends AutomataParserBaseListener {
-    private Stack<Integer> resultStack = new Stack<>();
+    private Stack<Float> resultStack = new Stack<>();
 
-    public Integer getResult() {
+    public Float getResult() {
         return resultStack.peek();
     }
 
     @Override
     public void exitMathExpressionSum(AutomataParser.MathExpressionSumContext ctx) {
-        Integer rightNumber = resultStack.pop();
-        Integer leftNumber = resultStack.pop();
+        Float rightNumber = resultStack.pop();
+        Float leftNumber = resultStack.pop();
 
-        Integer result = leftNumber + rightNumber;
+        Float result = leftNumber + rightNumber;
         resultStack.push(result);
     }
 
     @Override
     public void exitMathExpressionMinus(AutomataParser.MathExpressionMinusContext ctx) {
-        Integer rightNumber = resultStack.pop();
-        Integer leftNumber = resultStack.pop();
+        Float rightNumber = resultStack.pop();
+        Float leftNumber = resultStack.pop();
 
-        Integer result = leftNumber - rightNumber;
+        Float result = leftNumber - rightNumber;
+        resultStack.push(result);
+    }
+
+    @Override
+    public void exitMathExpressionTimes(AutomataParser.MathExpressionTimesContext ctx) {
+        Float rightNumber = resultStack.pop();
+        Float leftNumber = resultStack.pop();
+
+        Float result = leftNumber * rightNumber;
+        resultStack.push(result);
+    }
+
+    @Override
+    public void exitMathExpressionDivision(AutomataParser.MathExpressionDivisionContext ctx) {
+        Float rightNumber = resultStack.pop();
+        Float leftNumber = resultStack.pop();
+
+        Float result = leftNumber / rightNumber;
         resultStack.push(result);
     }
 
     @Override
     public void exitMathExpressionBasicNumber(AutomataParser.MathExpressionBasicNumberContext ctx) {
-        resultStack.push(Integer.valueOf(ctx.NUMBER().getText()));
+        resultStack.push(Float.valueOf(ctx.NUMBER().getText()));
     }
 }
