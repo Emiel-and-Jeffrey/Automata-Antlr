fde1ebbdb1c58c307ae194fb696c37aa553186c9
braggs <emielvdbrink@gmail.com>
2021-05-21 10:47:40 +0200

    added .jar artifact and cleaned up main

--- src/com/ap/automata/Main.java
+++ src/com/ap/automata/Main.java
@@ -8,10 +8,11 @@ import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.util.stream.Collectors;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         String input = "((4^2.2 - 3) * 3.2!) / 5 + 2";
 
         AutomataLexer lexer = new AutomataLexer(CharStreams.fromString(input));
@@ -21,12 +22,24 @@ public class Main {
         AutomataParser parser = new AutomataParser(tokens);
         ParseTree tree = parser.program();
 
-        //Trees.inspect(tree, parser);
-
         ParseTreeWalker walker = new ParseTreeWalker();
         AutomataParserListener listener = new AutomataParserListener();
         walker.walk(listener, tree);
 
-        System.out.println(listener.getResult());
+        String tokenString = tokens.getTokens()
+                                .stream()
+                                .map(token -> token.getText() + " ")
+                                .collect(Collectors.joining());
+        String treeString = tree.toStringTree(parser);
+        String result = listener.getResult().toString();
+
+        String[] msg = { "Input string ", "Lexed tokens ", "Generated tree ", "Result " };
+        String[] vals = { input, tokenString, treeString, result };
+
+        System.out.println("------------------------------------------------------");
+        for (int i = 0; i < msg.length; i++) {
+            System.out.printf("%22s: %10s%n", msg[i], vals[i]);
+        }
+        System.out.println("------------------------------------------------------");
     }
 }
