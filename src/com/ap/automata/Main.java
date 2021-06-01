da03028ac82e693579416c97283ad7e119eab40e
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2021-06-01 20:38:21 +0200

    Add basic file input

--- src/com/ap/automata/Main.java
+++ src/com/ap/automata/Main.java
@@ -2,24 +2,36 @@ package com.ap.automata;
 
 import com.ap.antlr.base.AutomataLexer;
 import com.ap.antlr.base.AutomataParser;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 import org.antlr.v4.runtime.tree.ParseTreeWalker;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-        //try out the following: ( ( 4^2.2 - 3 ) * 3.2! ) / 5 + 2
-        System.out.println("Please enter your equation:");
+        System.out.println("Please enter your filepath:");
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
+        File file = new File(input);
 
-        AutomataLexer lexer = new AutomataLexer(CharStreams.fromString(input));
+        if (!IsFileValid(file)) {
+            System.out.printf("File was not valid");
+            return;
+        }
+        CharStream targetStream = CharStreams.fromFileName(input);
+        HandleParsing(targetStream);
+    }
+
+    private static void HandleParsing(CharStream stream) {
+        AutomataLexer lexer = new AutomataLexer(stream);
 
         CommonTokenStream tokens = new CommonTokenStream(lexer);
 
@@ -37,13 +49,17 @@ public class Main {
         String treeString = tree.toStringTree(parser);
         String result = listener.getOutput();
 
-        String[] msg = {"Input string ", "Lexed tokens ", "Generated tree ", "Result "};
-        String[] vals = {input, tokenString, treeString, result};
+        String[] msg = {"Lexed tokens ", "Generated tree ", "Result "};
+        String[] vals = {tokenString, treeString, result};
 
         System.out.println("------------------------------------------------------");
         for (int i = 0; i < msg.length; i++) {
             System.out.printf("%22s: %10s%n", msg[i], vals[i]);
         }
         System.out.println("------------------------------------------------------");
+    }
+
+    private static boolean IsFileValid(File file) {
+        return file.exists() && !file.isDirectory();
     }
 }
