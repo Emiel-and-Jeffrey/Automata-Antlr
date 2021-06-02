75786b522f43fbd27e457b7d8fc235480d7a42a4
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2021-06-02 15:33:52 +0200

    Add visitor class

---
+++ src/com/ap/automata/AutomataParserVisitor.java
@@ -0,0 +1,14 @@
+package com.ap.automata;
+
+import com.ap.antlr.base.AutomataParser;
+import com.ap.antlr.base.AutomataParserBaseVisitor;
+
+public class AutomataParserVisitor extends AutomataParserBaseVisitor {
+    //todo
+
+    @Override
+    public Value visitPrint_expression(AutomataParser.Print_expressionContext ctx)
+    {
+
+    }
+}
